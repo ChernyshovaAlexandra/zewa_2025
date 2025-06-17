77003c7b685f8c7ae754cc43dd042f710f87a72e
Alexandra Chernyshova <alexandrachrystal@gmail.com>
2025-06-17 22:25:21 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-// vite.config.ts
-import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+const repoName = 'zewa_2025';
+
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    base: isProd
+      ? `/${repoName}/` // для GitHub Pages
+      : '/', // для npm run dev
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
 });
