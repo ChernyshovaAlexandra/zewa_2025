ca57be501b974640db153c021dd5619f10a39def
Alexandra Chernyshova <alexandrachrystal@gmail.com>
2025-06-17 21:46:45 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Base path for GitHub Pages deployment
-  base: '/zewa_2025/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
