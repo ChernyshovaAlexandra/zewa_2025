a877d7949d7c678928229a22453037cf20e5fb2f
Alexandra <alexandrachrystal@gmail.com>
2025-06-17 20:21:10 +0300

    refactor: add Telegram context and service

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import { StrictMode } from 'react';
+import { TelegramProvider } from './contexts/TelegramContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TelegramProvider>
+      <App />
+    </TelegramProvider>
   </StrictMode>,
 );
