cfc562b2658efe75b0a9403d349fe585666b6914
Alexandra Chernyshova <alexandrachrystal@gmail.com>
2025-11-10 15:23:35 +0700

    fix

--- src/features/onboarding/OnboardingScreen.tsx
+++ src/features/onboarding/OnboardingScreen.tsx
@@ -2,40 +2,14 @@ import { useRef, useState, type PointerEvent as ReactPointerEvent } from 'react'
 import { ZewaButton } from '@/shared/ui';
 import * as S from './OnboardingScreen.styles';
 import { applyNbsp } from '@/utils';
-
-export interface OnboardingStep {
-  image: string;
-  header: string;
-  text: string;
-}
+import { DEFAULT_ONBOARDING_STEPS, OnboardingStep } from './config';
 
 interface OnboardingScreenProps {
   onFinish: () => void;
   steps?: OnboardingStep[];
 }
 
-export const DEFAULT_ONBOARDING_STEPS: OnboardingStep[] = [
-  {
-    image: '/assets/images/onboarding/1_christmas.webp',
-    header: 'Скучали? Я вернулся!',
-    text: 'Перед Новым годом дел невпроворот. Но с Zewa дом сияет! Добавим немного волшебства. Поможете разобрать игрушки и нарядить ёлку?',
-  },
-  {
-    image: '/assets/images/onboarding/2_christmas.webp',
-    header: 'Не проЗЕВАй праздник!',
-    text: 'Покупайте Zewa в «Магните», побеждайте в игре, зарабатывайте снежинки, чтобы продвинуться по шкале призов, и получайте ценные подарки.',
-  },
-  {
-    image: '/assets/images/onboarding/3_christmas.webp',
-    header: 'Встречайте новую игру!',
-    text: 'Переверните все парные карточки, чтобы получить снежинки. Количество попыток не ограничено.',
-  },
-  {
-    image: '/assets/images/onboarding/4_christmas.webp',
-    header: 'Продвигайтесь по шкале призов',
-    text: 'Загружайте чеки с товарами Zewa, проходите 3 уровня игры каждую неделю и вступайте в Клуб помощников Домовёнка.',
-  },
-];
+
 
 const SWIPE_THRESHOLD = 40;
 
